fa41b1ce21ba5ca2bc3d7f0f302d80599e4df8ca
Johan Siebens <johan.siebens@gmail.com>
2021-09-23 13:17:55 +0200

    add env for socks5 server flag

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 /app/login.sh &
 
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:7055
+/app/tailscaled --tun=userspace-networking --socks5-server=${TAILSCALE_SOCKS5_SERVER:-localhost:1055}
