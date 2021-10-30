d85cef43d3f1388bf41e1fdf916d783a9f646cc2
Johan Siebens <johan.siebens@gmail.com>
2021-10-30 15:31:27 +0200

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} ${EXTRA_ARGS}
+until /app/tailscale --socket=/tmp/tailscaled.sock up --authkey=${TAILSCALE_AUTHKEY} ${EXTRA_ARGS}
 do
     sleep 0.1
 done
