a603a9bfcf15a25284063f4b10b3ba5039e7b798
Johan Siebens <johan.siebens@gmail.com>
2021-10-30 18:32:00 +0200

    update

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 /app/login.sh &
 
-/app/tailscaled --tun=userspace-networking --socks5-server=${TAILSCALE_SOCKS5_SERVER:-localhost:1055} --state=kube:${KUBE_SECRET} --socket=/tmp/tailscaled.sock
+/app/tailscaled --tun=userspace-networking --socks5-server=${TAILSCALE_SOCKS5_SERVER:-localhost:1055} --socket=/tmp/tailscaled.sock
